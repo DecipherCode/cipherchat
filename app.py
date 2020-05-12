0d85ddb158006ca91d42cdf4f43be2a212a8055c
suryachereddy <suryachereddy@gmail.com>
2020-05-12 15:24:35 +0530

    initial sql

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-    return render_template('index1.html')
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+    try:
+        username=request.form.get("username")
+        password=request.form.get("password")
+    except:
+        return render_template("error.html",message="error with username and password input")
+    
---
+++ models.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class users(db.Model):
+    __tablename__="users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    passhash = db.Column(db.String, nullable=False)
+
+
+
