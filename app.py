a5beceea4e0d935b42816d29d9233cdf89286a18
ZoroSenpai3 <raghubommana88@gmail.com>
2020-05-15 19:07:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,56 +9,71 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ltoxbcumoikvjg:5e0189eb9f387a3af26aec138c0c6ddae9b53c5885075693b0c4f11095e7938e@ec2-35-169-254-43.compute-1.amazonaws.com:5432/dcni7jifo8v66s"
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://rvlmycroyzfgkn:0eeb3d0d92220e66bbb4eb2c63f42a1ebdcd0c7bcba1c2a2f7fc268b5df20463@ec2-34-202-88-122.compute-1.amazonaws.com:5432/d6htgff90r5lfq"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 socketio = SocketIO(app)
 
 def main():
-    db.create_all()
+    db.createall()
 
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name")
+        res = request.form
+        print(res)
+        name = request.form.get("Name")
         username=request.form.get("username")
         gender=request.form.get("gender")
         password=request.form.get("password")
-        passhash=generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-        datetime = datetime.now()
+        passhash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        print(name,username,gender,password)
         if gender=='M':
             avatar_id = 0
         elif gender=='F':
             avatar_id = 4
         else:
             avatar_id = 8
-        print(datetime)
-        user = userdata(name=name,username=username,gender=gender,passhash=passhash,datetime=datetime,avatar_id = avatar_id)
+        user = userdata(name=name,username=username,gender=gender,passhash=passhash,avatar_id = avatar_id)
         db.session.add(user)
         db.session.commit()
         return render_template('index.html')
     else:
         return render_template('index.html')
 
-@app.route("/home" ,methods=["POST"])
-def home():
+#@app.route("/home" ,methods=["POST"])
+#def home():
+#    username=request.form.get("username")
+#    password=request.form.get("password")
+    #user = users.query.filter_by(username=username).first()
+    #if user is None:
+    #    return render_template('construction.html',message="Invalid username or password")
+    #else:
+    #    if check_password_hash(user.passhash,password):
+    #        return render_template("home.html")
+    #    else:
+    #        return "Wrong Password"
+
+@app.route("/register",methods=["POST"])
+def register():
     username=request.form.get("username")
     password=request.form.get("password")
-    user = users.query.filter_by(username=username).first()
-    if user is None:
-        return render_template('construction.html',message="Invalid username or password")
-    else:
-        if check_password_hash(user.passhash,password):
-            return render_template("home.html")
-        else:
-            return "Wrong Password"
+    #check=users.query.filter_by(username=username).first()
+    #if check is None and (len(username)>4 or len(password)>4) :
+    #    passhash=generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+    #    add=users(username=username,passhash=passhash)
+    #    db.session.add(add)
+    #    db.session.commit()
+    #    return render_template('construction.html',message="Account successfully created please go back and try to login :)")
+    #else:
+    #    return render_template('construction.html',message="Use another username/password. Maybe username or password is too short?. Or maybe the username is already taken")
 
-@app.route("/API/usernames",methods = ["POST","GET"])
-def usernames():
-    usernames = db.session.query(users.username).all()
-    u_list = [usernames[i][0] for i in range(len(usernames))]
-    return jsonify({"users":u_list})
+#@app.route("/API/usernames",methods = ["POST","GET"])
+#def usernames():
+    #usernames = db.session.query(users.username).all()
+    #u_list = [usernames[i][0] for i in range(len(usernames))]
+#    return jsonify({"users":u_list})
 
 if __name__ == "__main__":
 # Allows for command line interaction with Flask application
