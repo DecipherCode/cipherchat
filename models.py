b9823fcdb827c326099b09f958228941b5bb87fc
ZoroSenpai3 <raghubommana88@gmail.com>
2020-05-15 19:55:21 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
+import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 
-class users(db.Model):
-    __tablename__="users"
+class userdata(db.Model):
+    __tablename__="userdata"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String(1), nullable=False)
     passhash = db.Column(db.String, nullable=False)
+    datetime = db.Column(db.DateTime(timezone=True), default=func.now())
+    avatar_id = db.Column(db.Integer, nullable=False)
 
